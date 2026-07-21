e97fd5c05ab4777416b8faf053e7bac5df212f14
Kyou <fianlbass14@gmail.com>
2026-07-21 13:21:29 +0700

    edit lab.py

--- student-work/phase-00/01-first-python/lab.py
+++ student-work/phase-00/01-first-python/lab.py
@@ -1,4 +1,6 @@
-import torch
-print(f"CUDA available: {torch.cuda.is_available()}")
-if torch.cuda.is_available():
-    print(f"GPU: {torch.cuda.get_device_name(0)}")
+# import torch
+# print(f"CUDA available: {torch.cuda.is_available()}")
+# if torch.cuda.is_available():
+#     print(f"GPU: {torch.cuda.get_device_name(0)}")
+    
+print("Hello World")
